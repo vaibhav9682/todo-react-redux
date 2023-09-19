74e1356aca0508783b6742ce9889e11395a3f735
vaibhav9682 <vaibhav205.vd@gmail.com>
2023-09-19 12:20:23 +0530

    added redux toolkit

---
+++ src/components/ToDoList/ToDoList.js
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTodo } from "../../redux/actions/todoActions";
+
+import styles from "./ToDoList.module.css";
+
+function ToDoList() {
+
+  const todos=useSelector((state)=> state.todoReducer.todos);
+  console.log(todos);
+  const disptach = useDispatch();
+  // const todos= store.getState().todos;
+
+  return (
+    <div className={styles.container}>
+    <ul>
+      {todos.map((todo,index) => (
+        <li className={styles.item} key={todo.id}>
+          <span className={styles.content}>{todo.text}</span>
+          <span className={todo.completed ? styles.completed:styles.pending}>{todo.completed ? 'Completed': 'Pending'}</span>
+          <button className="btn btn-warning"
+          onClick={()=>{disptach(toggleTodo(index))}}
+          >Toggle</button>
+          </li>
+      ))}
+    </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
